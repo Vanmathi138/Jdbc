e63ec6fbe46164094bac7d06052c023c08a0f666
Vanmathi Karunanithi <150342354+Vanmathi138@users.noreply.github.com>
2025-11-13 12:40:38 +0530

    use case 6

--- src/main/java/com/app/JDBC/EmployeePayrollService.java
+++ src/main/java/com/app/JDBC/EmployeePayrollService.java
@@ -6,37 +6,47 @@ import java.util.List;
 
 import com.app.JDBC.exception.CustomDatabaseException;
 
+import java.sql.*;
+import java.util.*;
+
+import com.app.JDBC.exception.CustomDatabaseException;
+
 public class EmployeePayrollService {
+
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/payroll_service";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "V@an123&";
 
-    public List<EmployeePayroll> getEmployeesInDateRange(LocalDate start, LocalDate end) throws CustomDatabaseException {
-        List<EmployeePayroll> employeeList = new ArrayList<>();
-        String query = "SELECT * FROM employee_payroll WHERE startDate BETWEEN ? AND ?"; 
+    public void getSalaryStatisticsByGender() throws CustomDatabaseException {
+        String query = "SELECT gender, SUM(salary) AS total_salary, " +
+                       "AVG(salary) AS average_salary, MIN(salary) AS min_salary, " +
+                       "MAX(salary) AS max_salary, COUNT(*) AS count_employees " +
+                       "FROM employee_payroll " +
+                       "GROUP BY gender";
 
         try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+             PreparedStatement ps = connection.prepareStatement(query);
+             ResultSet rs = ps.executeQuery()) {
 
-            preparedStatement.setDate(1, java.sql.Date.valueOf(start));
-            preparedStatement.setDate(2, java.sql.Date.valueOf(end));
+            while (rs.next()) {
+                String gender = rs.getString("gender");
+                double sum = rs.getDouble("total_salary");
+                double avg = rs.getDouble("average_salary");
+                double min = rs.getDouble("min_salary");
+                double max = rs.getDouble("max_salary");
+                int count = rs.getInt("count_employees");
 
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                double salary = resultSet.getDouble("salary");
-                LocalDate startDate = resultSet.getDate("startDate").toLocalDate();
-
-                employeeList.add(new EmployeePayroll(id, name, salary, startDate));
+                System.out.println("Gender: " + gender);
+                System.out.println("Total Salary: " + sum);
+                System.out.println("Average Salary: " + avg);
+                System.out.println("Minimum Salary: " + min);
+                System.out.println("Maximum Salary: " + max);
+                System.out.println("Number of Employees: " + count);
+                System.out.println("----------------------------");
             }
 
         } catch (SQLException e) {
-            throw new CustomDatabaseException("Error retrieving employees by date range", e);
+            throw new CustomDatabaseException("Error retrieving salary statistics by gender", e);
         }
-
-        return employeeList;
     }
-
 }
--- src/main/java/com/app/JDBC/JdbcApplication.java
+++ src/main/java/com/app/JDBC/JdbcApplication.java
@@ -17,21 +17,11 @@ public class JdbcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcApplication.class, args);
 		EmployeePayrollService dbService = new EmployeePayrollService();
-
-        LocalDate startDate = LocalDate.of(2022, 12, 31);
-        LocalDate endDate = LocalDate.of(2024, 8, 13);
-
-        try {
-            List<EmployeePayroll> employees = dbService.getEmployeesInDateRange(startDate, endDate);
-
-            System.out.println("Employees who joined between " + startDate + " and " + endDate + ":");
-            for (EmployeePayroll emp : employees) {
-                System.out.println(emp);
-            }
-
-        } catch (CustomDatabaseException e) {
-            e.printStackTrace();
-        }
+	    try {
+	        dbService.getSalaryStatisticsByGender();
+	    } catch (CustomDatabaseException e) {
+	        e.printStackTrace();
+	    }
 	}
 
 }
